87cabb3b045175a198009b482dc6f7216ce97421
gorbov <usachborodach@gmail.com>
2026-02-15 22:18:26 +0300

    modified:   client.py 	new file:   history.log

--- nohead/client.py
+++ nohead/client.py
@@ -9,26 +9,34 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 config = json.loads(open("config.json", encoding='UTF-8').read())
 
+history = open('history.log', encoding="UTF-8").read()
+print(history)
+
+
 @app.route('/send_message/', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
-def handle_all():
+def listen():
     received_data = request.get_json()
-    message = f"{received_data['author']}: {received_data['text']}"
-    print(message)
+    in_message = f"{received_data['author']}: {received_data['text']}"
+    with open('history.log', 'a', encoding='UTF-8') as file:
+        file.write(in_message)
     return 'done'
 
 def run_server():
     app.run(host='0.0.0.0', port=5000, debug=False)
-
-
-
 threading.Thread(target=run_server, daemon=True).start()
-print(f'{__name__} запущен')
+
+
 
 while True:
     text = input()
     sys.stdout.write('\033[F')
     sys.stdout.write('\033[K')
-    print(f"{config['author']}: {text}")
+
+    out_message = f"{config['author']}: {text}"
+    print(out_message)
+    with open('history.log', 'a', encoding='UTF-8') as file:
+        file.write(out_message)
+
     data_to_send = {
         'text': text,
         'author': config['author'],
